7a80eca88fed415adc9721a3041c959e027a42d5
Lukasz Dziedziczak <wukash@gmail.com>
2025-02-17 13:22:57 +1030

    Added Character States

---
+++ SpaceServo/Assets/Scripts/Characters/States/CS_Idle.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CS_Idle : CharacterState
+{
+    public CS_Idle(Character character) : base(character)
+    {
+
+    }
+
+    public override void StateEnd()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void StateStart()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void StateTick()
+    {
+        throw new System.NotImplementedException();
+    }
+}
